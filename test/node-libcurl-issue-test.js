f79047d83cb057c1f6c34d885bca01f247b04fb4
sznowicki <kontakt@szymon-nowicki.pl>
2019-06-18 22:56:24 +0200

    Example test for the github issue

---
+++ test/node-libcurl-issue-test.js
@@ -0,0 +1,31 @@
+const { Curl } = require('node-libcurl');
+
+const makeCall = () => new Promise((resolve, reject) => {
+  const curl = new Curl();
+
+  curl.setOpt('URL', 'https://example.com');
+
+  curl.on('error', () => reject());
+
+  curl.on('end', () => {
+    console.log('END END END, now resolve');
+    resolve()
+  });
+
+  curl.perform();
+});
+
+
+describe('Promisified curl call test', () => {
+  it('should resolve', (done) => {
+    makeCall()
+      .then(() => {
+        console.warn('TEST DONE');
+        done();
+      })
+      .catch(() => {
+        console.warn('TEST CATCHED ERROR');
+        done();
+      })
+  });
+});
