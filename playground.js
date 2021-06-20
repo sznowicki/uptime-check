e99ab121fdf518f4f822aa2165b610f20318210f
Szymon Nowicki <sznowicki@eskalacja.com>
2021-06-20 22:18:33 +0200

    Add playground console log

--- playground.js
+++ playground.js
@@ -3,11 +3,13 @@ const check = require('./index');
 const main = async () => {
   try {
     console.log('Bad SSL');
-    await check({
+    const badSSL = await check({
       url: 'https://expired.badssl.com',
       keyword: 'agentslugdsfdsfsfsdfd',
       redirectsLimit: 2
     })
+
+    console.log(badSSL.status, badSSL.httpCode);
 
     const result = await check({
       url: 'https://agentslug.com',
