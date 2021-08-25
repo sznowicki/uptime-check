2b4c719a812acb9b174437e3d416a9f056b48673
FxllenCode <ethan@fxllencode.dev>
2021-08-25 08:20:38 -0400

    Update index.js
    Co-authored-by: Szymon Nowicki <sznowicki@users.noreply.github.com>

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const mergeDefaults = (opts) => {
     keyword: null,
     redirectsLimit: 3,
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+      'User-Agent': 'Uptime-check - https://www.npmjs.com/package/uptime-check',
     },
     timeOut: 10
   };
