d8a980c057e293a38534ddfded2b1072811d5a73
Szymon Nowicki <sznowicki@eskalacja.com>
2021-08-25 21:03:14 +0200

    Clean up playground

--- playground.js
+++ playground.js
@@ -2,14 +2,13 @@ const check = require('./index');
 
 const main = async () => {
   try {
-    console.log('Bad SSL');
     const badSSL = await check({
       url: 'https://expired.badssl.com',
       keyword: 'agentslugdsfdsfsfsdfd',
       redirectsLimit: 2
-    })
+    });
 
-    console.log(badSSL.status, badSSL.httpCode, badSSL);
+    console.log(badSSL);
 
     const result = await check({
       url: 'https://agentslug.com/dsfsdfsd',
@@ -17,10 +16,9 @@ const main = async () => {
       redirectsLimit: 2
     });
 
-    console.log(result, result.httpCode);
-
+    console.log(result);
   } catch (error) {
-
+    console.log(error);
   }
 };
 
